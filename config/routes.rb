e529f01226ca189b35e456dd6ed7fcd6f8bd2f32
Harry Ng <harryworld@gmail.com>
2014-11-13 05:44:07 +0800

    Makeup for the API features

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :lunches
+  scope :api do
+    resources :lunches, only: [:index, :show, :create]
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
