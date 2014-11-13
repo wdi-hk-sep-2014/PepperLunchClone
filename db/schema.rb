29200595f8947c79bf1a5d88d1a634c1166f8e90
Nate <primesitemedia@gmail.com>
2014-11-13 10:58:33 +0800

    redo scaffold Lunch, add TBR gem

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141112031801) do
+ActiveRecord::Schema.define(version: 20141113025540) do
 
   create_table "lunches", force: true do |t|
     t.string   "name"
