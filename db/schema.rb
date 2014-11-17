00480eca72824f28d3153c376b443e424994f9aa
Fer Martin <fernandomartinsantana@gmail.com>
2014-11-17 23:14:15 +0800

    switching database to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141118013052) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "lunches", force: true do |t|
     t.string   "name",       default: "PepperLunch"
     t.string   "location",   default: "Cyberport"
