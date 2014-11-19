d5b1ce8cf820d001fb17484dc4aa248dd008916c
Mark Wilson <secretchinamark@gmail.com>
2014-11-19 17:47:02 +0800

    Require login, associate lunches to current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+  # Require login to access any action
+  before_filter :authenticate_user!
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
