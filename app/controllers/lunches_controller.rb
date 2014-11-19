818211776a5e7f22e5d886adc0075216cae9a1ef
Mark Wilson <secretchinamark@gmail.com>
2014-11-19 22:43:55 +0800

    Send the user an email when they update their lunch data

--- app/controllers/lunches_controller.rb
+++ app/controllers/lunches_controller.rb
@@ -14,6 +14,9 @@ class LunchesController < ApplicationController
         end
       end
     end
+
+    LunchMailer.data_update_notification(current_user).deliver
+
     render status: :created
   end
 
